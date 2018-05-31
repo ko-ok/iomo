60ae9b1af349a8128084054502b62a9d031f7689
Oskar Niburski <oskarniburski@gmail.com>
2018-05-30 23:12:43 -0700

    feat(app): App build

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import iomo from './iomo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <canvas id="canvas"></canvas>
+        <div id="text">
+          <img src={iomo} className="small-gear gear-yellow" alt="gear-2" />
+        </div>
+
+        <div id="apply">
+          <a href="mailto:iomo@email.com?Subject=Hello">Apply</a>
+        </div>
       </div>
     );
   }
